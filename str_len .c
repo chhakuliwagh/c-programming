4260daf2a0d53bcfa15fd37a4a2f2538eda8ed33
chhakuliwagh <dipaliw24@gmail.com>
2025-09-26 12:36:26 +0530

    string

---
+++ str_len .c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<string.h>
+
+ /*void main()
+{
+	char name[20];
+	printf("Enter Your Name:");
+	gets(name);
+//	printf("Enter Your String:");
+//	scanf("%d",&name[i]);
+	printf("Length is %d",strlen(name));
+}*/
+
+
+
+ /*void main()
+ {
+	char name1[20]="Anand Ogale";
+	char name2[20];
+	strcpy(name2,name1);
+	printf("Name 1=%s \n",name1);
+	printf("Name 2=%s \n",name2);
+	
+}*/
+
+/*void main()
+ {
+	char name1[20];
+	char name2[20];
+	printf("Enter Your Name:");
+	gets(name1);
+	strcpy(name2,name1);
+	printf("Name 1=%s \n",name1);
+	printf("Name 2=%s \n",name2);	
+}*/
+
+/*void main()
+{
+	char s1[10]="Dipali";
+	char s2[20]="Dipali";
+	if(strcmp(s1,s2)==0)
+		printf("String are Equal");
+	else
+	printf("String are not Equal");
+}*/
+
+/*void main()
+{
+	char s1[10];
+	printf("Enter Your Name1;");
+	gets(s1);
+	char s2[20];
+	printf("Enter Your Name1;");
+	gets(s2);
+	if(strcmp(s1,s2)==0)
+	printf("String are Equal");
+	else
+	printf("String are not Equal");
+}*/
+
+/*void main()
+{
+	char s1[10]="Dipali";
+	char s2[20]="dipali";
+	if(strcasecmp(s1,s2)==0)
+		printf("String are Equal");
+	else
+	printf("String are not Equal");
+}*/
+
+/*void main()
+{
+	char password[20],input[20];
+	strcpy(password,"admin123");
+	
+	printf("Enter Password\n");
+	scanf("%s",&input);
+	
+	if(strcmp(password,input)==0)
+	printf("Access Granded");
+	else
+	printf("Access Denied");
+}*/
